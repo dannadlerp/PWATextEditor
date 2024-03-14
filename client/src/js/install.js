52e3c5942fde21e6864fc1a5248c1fc98c15b432
Dan Nadler <dan.nadlerp@gmail.com>
2024-03-14 15:06:41 -0400

    added functionality to install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,29 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  window.promptEvent = event;
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.promptEvent;
+  if (!promptEvent) {
+    return;
+  }
+  promptEvent.prompt();
+  const { outcome } = await promptEvent.userChoice;
+  if (outcome === "accepted") {
+    console.log("User accepted the A2HS prompt");
+  } else {
+    console.log("User dismissed the A2HS prompt");
+  }
+  window.promptEvent = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  window.promptEvent = null;
+  console.log("app installed");
+});
